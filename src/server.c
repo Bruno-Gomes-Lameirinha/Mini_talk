297e2f100be3fcbfb04a76a4b39ab42223098dae
Bruno Gomes Lameirinha <bgomes-l@c1r5p1.42sp.org.br>
2024-02-01 20:23:44 -0300

    sigaction

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: bgomes-l <bgomes-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:37:26 by bgomes-l          #+#    #+#             */
-/*   Updated: 2024/02/01 18:37:33 by bgomes-l         ###   ########.fr       */
+/*   Updated: 2024/02/01 20:21:36 by bgomes-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 
 int	main(void)
 {
-	ft_printf("PID: %d\n PID: %d\n", getpid(), getpid());
+	struct sigaction sa;
+
+	sa.sa_sigaction = recive_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	ft_printf("PID: %d \n", getpid());
+	while (1)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+	}
+	ft_printf("\n");
+	return (0);
 }
